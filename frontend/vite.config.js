05df9d57b4f4408b891f917084ae308050aece3f
tanux22 <atanushreddy@gmail.com>
2025-11-06 15:29:24 +0530

    deployment bug fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,8 @@
     // https://vite.dev/config/
     export default defineConfig({
       plugins: [react(), tailwindcss(), ],
+      define:{
+        __BACKEND_URL__: JSON.stringify(process.env.VITE_BACKEND_URL || "https://neuranotes-backend-ert6.onrender.com"),
+        __LANGCHAIN_FASTAPI_URL__: JSON.stringify(process.env.VITE_LANGCHAIN_FASTAPI_URL || "https://langchain-neuranotes.onrender.com"),
+      }
     })
